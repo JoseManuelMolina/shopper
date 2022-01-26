0b9ab16e516bd7d27f6c80dad24da959657ae717
Jose Manuel <jose.manuel.molina2001@gmail.com>
2022-01-26 20:43:29 +0100

    Añadir contenido estatico

--- proyectoFinal/diverse/urls.py
+++ proyectoFinal/diverse/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     #Deja la cadena vacía para usar la url base (/)
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="cheackout"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- proyectoFinal/proyectoFinal/settings.py
+++ proyectoFinal/proyectoFinal/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
