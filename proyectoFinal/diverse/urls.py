449d789ef3212fda217218e991d140d79630d92c
Jesus <jesuscabellomanzano2001@gmail.com>
2022-01-29 22:32:33 +0100

    Principio - Crear Color Backend

--- proyectoFinal/diverse/urls.py
+++ proyectoFinal/diverse/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from . import viewsBackend
 from . import views
 
+from diverse.viewsBackend import *
+from diverse.views import *
+
 urlpatterns = [
     #Deja la cadena vacía para usar la url base (/)
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('backend/', viewsBackend.index, name="indexBackend"),
+    path('backend/', viewsBackend.indexList, name="indexBackend"),
+    path('backend/crear', viewsBackend.crearColor, name="crearColor"),
 ]
