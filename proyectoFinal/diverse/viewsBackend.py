e3d2dbf5f721d1b65eef588ea90f375f7ee646df
Jesus <jesuscabellomanzano2001@gmail.com>
2022-01-30 19:26:06 +0100

    Backend - Crear Color 1

--- proyectoFinal/diverse/viewsBackend.py
+++ proyectoFinal/diverse/viewsBackend.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+
+from diverse.forms import *
+
+from .models import *
 
 # Create your views here.
 
 # BACKEND
 
-def index(request):
-    context = {}
-    return render(request, 'diverseBackend/index.html', context)
+# class indexListView(ListView):
+#    model = color
+#    template_name = 'index.html'
+#    content_object_name = 'indexList'
+
+def indexList(request):
+    colores = color.objects.all()
+    return render(request, 'diverseBackend/index.html', {'colores': colores})
+
+#@login_required(login_url='/usuario/login/')
+def crearColor(request):
+    if request.method == 'POST':
+        form = colorForm(request.POST)
+        if form.is_valid():
+            colorDatosForm = form.cleaned_data
+
+            colorDatos = color(
+                nombre = colorDatosForm['nombreColor'],
+            )
+
+            colorDatos.save()
+        return redirect('indexBackend')
+    else:
+        form = colorForm()
+    
+    return render(request, 'diverseBackend/color_form.html', {'form':form})
