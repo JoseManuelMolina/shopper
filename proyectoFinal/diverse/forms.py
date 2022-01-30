b380655bfca047845367d9e89e10df14b2293ac4
Jesus <jesuscabellomanzano2001@gmail.com>
2022-01-30 19:51:44 +0100

    Backed - Forms.py & color_form.html

---
+++ proyectoFinal/diverse/forms.py
@@ -0,0 +1,20 @@
+from tkinter import Widget
+from turtle import width
+from unittest.util import _MAX_LENGTH
+from django import forms
+
+from .models import *
+
+class colorForm(forms.Form):
+
+    nombreColor = forms.CharField(
+        max_length=60,
+        label='nombreColor',
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control', 
+                'id':'nombreColor', 
+                'placeholder':'Nombre Color'}
+        )
+    )
+
