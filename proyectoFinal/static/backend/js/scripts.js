0d32db8fef1b35b6734c63b0e7acc2ae01a8f7c2
Jesus <jesuscabellomanzano2001@gmail.com>
2022-05-05 20:51:12 +0200

    Cambios Sidebar

--- proyectoFinal/static/backend/js/scripts.js
+++ proyectoFinal/static/backend/js/scripts.js
@@ -1,25 +1,46 @@
 function botonDropdown(id) {
 
     if (id == "crear") {
-        if (document.getElementById("mas-crear").classList.contains("display-none")) {
-            document.getElementById("mas-crear").classList.remove("display-none");
-            document.getElementById("menos-crear").classList.add("display-none");
-            document.getElementById("crearDropdown").classList.add("display-none");
+        if (document.getElementById("mas-crear").classList.contains("activo")) {
+            document.getElementById("mas-crear").classList.remove("activo");
+            document.getElementById("mas-crear").classList.remove("rotate-90-ccw");
+            document.getElementById("crearDropdown").classList.remove("mostrarDropdown");
+            document.getElementById("mas-crear").classList.add("rotate-90-cw");
+            document.getElementById("crearDropdown").classList.add("esconderDropdown");
         } else {
-            document.getElementById("mas-crear").classList.add("display-none");
-            document.getElementById("menos-crear").classList.remove("display-none");
-            document.getElementById("crearDropdown").classList.remove("display-none");
+            document.getElementById("mas-crear").classList.add("activo");
+            document.getElementById("mas-crear").classList.remove("rotate-90-cw");
+            document.getElementById("crearDropdown").classList.remove("esconderDropdown");
+            document.getElementById("mas-crear").classList.add("rotate-90-ccw");
+            document.getElementById("verDropdown").classList.add("mostrarDropdown");
         }
     }else{
-        if (document.getElementById("mas-ver").classList.contains("display-none")) {
-            document.getElementById("mas-ver").classList.remove("display-none");
-            document.getElementById("menos-ver").classList.add("display-none");
-            document.getElementById("verDropdown").classList.add("display-none");
+        if (document.getElementById("mas-ver").classList.contains("activo")) {
+            document.getElementById("mas-ver").classList.remove("activo");
+            document.getElementById("mas-ver").classList.remove("rotate-90-ccw");
+            document.getElementById("verDropdown").classList.remove("mostrarDropdown");
+            document.getElementById("mas-ver").classList.add("rotate-90-cw");
+            document.getElementById("verDropdown").classList.add("esconderDropdown");
         } else {
-            document.getElementById("mas-ver").classList.add("display-none");
-            document.getElementById("menos-ver").classList.remove("display-none");
-            document.getElementById("verDropdown").classList.remove("display-none");
+            document.getElementById("mas-ver").classList.add("activo");
+            document.getElementById("mas-ver").classList.remove("rotate-90-cw");
+            document.getElementById("verDropdown").classList.remove("esconderDropdown");
+            document.getElementById("mas-ver").classList.add("rotate-90-ccw");
+            document.getElementById("verDropdown").classList.add("mostrarDropdown");
         }
     }
 
+}
+
+function checkboxBackend() {
+    var checkbox = document.getElementById("checkboxCrear");
+    
+    if(checkbox.checked == true){ 
+        $('#checkboxCrear').attr('checked', false);
+        console.log('activo');
+    }
+    else{ 
+        $('#checkboxCrear').attr('checked', true);
+        console.log('inactivo');
+    }
 }
